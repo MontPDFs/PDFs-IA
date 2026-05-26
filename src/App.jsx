f0a5041347b95de99eb55e146f4165cb8fcb99a0
MontPDFs <info.montpdf@gmail.com>
2026-05-25 22:08:11 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Prompts from './pages/Prompts'
+import Research from './pages/Research'
 import Generator from './pages/Generator'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/crear" element={<Generator />} />
         <Route path="/prompts" element={<Prompts />} />
+        <Route path="/investigar" element={<Research />} />
       </Routes>
     </BrowserRouter>
   )
